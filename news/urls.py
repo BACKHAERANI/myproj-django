decce8265c54eac31e4c134977e463d3b948ed05
back hae ran <idg9833@gmail.com>
2022-01-12 10:37:28 +0900

    app 'news'//DRF를 활용한 API 만들기

--- news/urls.py
+++ news/urls.py
@@ -1,3 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from news.views import ArticleViewSet, article_list, article_new
+
 app_name ='news'
 
-urlpatterns = []
+
+router = DefaultRouter()
+router.register("article", ArticleViewSet)
+
+urlpatterns = [path('article/', article_list, name="article_list"),
+               path('article/new/', article_new, name="article_new"),
+               path('api/', include(router.urls))]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from rest_framework import viewsets
+from news.forms import ArticleForm
+from news.models import Article
+from news.serializers import ArticleSerializer
 
-# Create your views here.
+article_list = ListView.as_view(model=Article)
+
+article_new = CreateView.as_view(model=Article, form_class=ArticleForm, success_url=reverse_lazy("news:article_list"))
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
