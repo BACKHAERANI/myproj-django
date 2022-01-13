5c035c3ea0c0ec12577f3cda5c39a461f15e4eac
back hae ran <idg9833@gmail.com>
2022-01-13 18:08:42 +0900

    models // TimestampedModel 지정 실수 수정

--- news/models.py
+++ news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import  MinLengthValidator, RegexValidator
+from django.core.validators import MinLengthValidator, RegexValidator
 
 
 class TimestampedModel(models.Model):
@@ -10,11 +10,14 @@ class TimestampedModel(models.Model):
         abstract = True
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=100,
-                             db_index=True,
-                             validators=[MinLengthValidator(3),
-                                         RegexValidator(r"[ㄱ-힣]", message="한글을 입력해주세요.")])
+class Article(TimestampedModel):
+    title = models.CharField(
+        max_length=100,
+        db_index=True,
+        validators=[
+            MinLengthValidator(3),
+            RegexValidator(r"[ㄱ-힣]", message="한글을 입력해주세요."),
+        ],
+    )
     content = models.TextField()
     photo = models.ImageField(blank=True)
-
