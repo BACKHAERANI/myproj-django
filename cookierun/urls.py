a0cf356b13c027d90136e49dced6f4055ecf30a5
back hae ran <idg9833@gmail.com>
2022-01-14 10:37:09 +0900

    app cookierun api 생성

--- cookierun/urls.py
+++ cookierun/urls.py
@@ -1,3 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from cookierun.views import CharacterViewSet
+
 app_name = "cookierun"
 
-urlpatterns = []
+
+router = DefaultRouter()
+
+
+router.register("characters", CharacterViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
--- cookierun/views.py
+++ cookierun/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
+from cookierun.models import Character
+from cookierun.serializers import CharacterSerializer
 
-# Create your views here.
+
+class CharacterViewSet(viewsets.ModelViewSet):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
